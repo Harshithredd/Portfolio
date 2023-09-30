2aecd23d29bc1069c7936cfa53454eda3867f27f
harshith <harshithreddy86@gmail.com>
2023-09-30 11:43:13 +0530

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,31 @@
+import NavBar from '@/components/NavBar/NavBar'
 import '@/styles/globals.css'
+import { Montserrat, Inter, Roboto_Mono } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  variable: '--font-roboto-mono',
+  display: 'swap',
+})
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont"
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${montserrat.variable} ${roboto_mono.variable} font-mont`}>
+      <NavBar />
+      <Component {...pageProps} />
+
+
+    </main>
+  )
 }
