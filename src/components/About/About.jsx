8a1726924536d363c98b5573dd56be7853c041ad
harshith <harshithreddy86@gmail.com>
2023-11-13 21:43:10 +0530

    can be escaped with &apos fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -28,18 +28,18 @@ const About = () => {
               Biography
             </h2>
             <p className="font-medium">
-              Hi, I'm Harshith G S, a web developer with a passion for creating
+              Hi, I&apos;m Harshith G S, a web developer with a passion for creating
               beautiful, functional, and user-centered digital experiences. With
               2 years of experience in the field. I am always looking for new
-              and innovative ways to bring my clients' visions to life.
+              and innovative ways to bring my client&apos;s visions to life.
             </p>
             <p className="font-medium my-4">
               I believe that design is about more than just making things look
-              pretty – it's about solving problems and creating intuitive,
+              pretty – it&apos;s about solving problems and creating intuitive,
               enjoyable experiences for users.
             </p>
             <p className="font-medium ">
-              Whether I'm working on a website or other digital product, I bring
+              Whether I&apos;m working on a website or other digital product, I bring
               my commitment to design excellence and user-centered thinking to
               every project I work on. I look forward to the opportunity to
               bring my skills and passion to your next project.
